0edd1f2c8a09223c9f8a4096c172272d956282e7
Sugansdv <suganyasdv16@gmail.com>
2025-07-20 03:21:21 +0530

    add

--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -49,7 +49,7 @@ const RegistrationForm = () => {
     } else {
       const { confirmPassword, ...userData } = formData;
       try {
-        const res = await axios.post("http://localhost:5000/api/register", userData);
+        const res = await axios.post("https://todoapp-xf5i.onrender.com/api/register", userData);
         if (res.status === 201) {
           setShowModal(true);
           setFormData({
